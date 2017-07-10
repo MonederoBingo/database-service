200dee7d123384a8c8c95193c0f79d7f8cd98a40
alayor <alayor3@gmail.com>
2017-07-09 20:41:30 -0500

    Adding support to run flyway migration in deployment env.

---
+++ src/main/java/com/monederobingo/database/migrations/Migrate.java
@@ -0,0 +1,38 @@
+package com.monederobingo.database.migrations;
+
+import org.flywaydb.core.Flyway;
+
+public class Migrate
+{
+    private final static String[] schemas = new String[] { "monedero", "monedero_test" };
+
+    public static void main(String[] args)
+    {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(getUrl(), getUsername(), getPassword());
+
+        for (String schema : schemas)
+        {
+            flyway.setSchemas(schema);
+            flyway.migrate();
+        }
+    }
+
+    private static String getUrl()
+    {
+        String url = System.getenv("JDBC_DATABASE_URL");
+        return url != null ? url : "jdbc:postgresql://localhost:5432";
+    }
+
+    private static String getUsername()
+    {
+        String user = System.getenv("JDBC_DATABASE_USERNAME");
+        return user != null ? user : "postgres";
+    }
+
+    private static String getPassword()
+    {
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+        return password != null ? password : "root";
+    }
+}
